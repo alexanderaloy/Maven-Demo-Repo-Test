3472f79a3ff04a193d6136381f0ca7cac04601ab
ALEXANDER <ALEXANDER@DESKTOP-LIM5PU2>
2025-01-23 10:57:29 +0530

    Code Push

---
+++ MavenDemoProject/src/main/java/demoPackage/TestClass.java
@@ -0,0 +1,32 @@
+package demoPackage;
+
+/*
+public class TestClass {
+	
+	public TestClass(String s , int a) {
+	System.out.println(s + ", "+a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestClass obj = new TestClass("Test Value",5);
+	}
+
+}
+*/
+
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestClass obj = new TestClass();
+		obj.displayvalues("testvalue",5);
+	}
+	
+	public void displayvalues(String s , int a) {
+	System.out.println(s + ", "+a);
+	}
+
+}
+
